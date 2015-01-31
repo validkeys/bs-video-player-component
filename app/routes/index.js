a603f6906bade101b3a0236bcb9e2e25655fdd24
Kyle Davis <validkeys@gmail.com>
2015-01-31 13:02:56 -0500

    no message

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,12 +6,7 @@ export default Ember.Route.extend({
     return {
       src:    "http://buildsociety.s3.amazonaws.com/video-component/video-component-demo.mp4",
       width:  1280,
-      height: 720,
-      comments: [
-        {id: 1, name: "Sara", time: 10, comment: "Something happens at 10 seconds"},
-        {id: 2, name: "Lucie", time: 20, comment: "Something happens at 20 seconds"},
-        {id: 3, name: "Kyle", time: 30, comment: "Something happens at 30 seconds"}
-      ]
+      height: 720
     };
   }
 
